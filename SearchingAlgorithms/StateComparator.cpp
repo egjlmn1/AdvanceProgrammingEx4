d5a25249bdbd562d8ec1d1dda7c638799fd6952e
ophircarmel <ophir.carmel@gmail.com>
2020-01-29 13:22:34 +0200

    bug fixes

--- SearchingAlgorithms/StateComparator.cpp
+++ SearchingAlgorithms/StateComparator.cpp
@@ -6,5 +6,6 @@
 
 template <class T>
 bool StateComparator<T>::operator()(State<T> *s1, State<T> *s2) {
-    return (s1->GetCost() > s2->GetCost());
+    return (s1->GetCost() >
+    s2->GetCost());
 }
